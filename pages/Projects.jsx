4ff78dd33c970666e780452899008421fbda487a
Joe Biggica <joe.biggica@thomsonreuters.com>
2020-07-24 12:50:47 -0400

    Updates

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -93,8 +93,7 @@ class Projects extends Component {
 		return (
 			<>
 				<Header 
-					router={router} 
-					gradient
+					router={router}
 					position={Header.Position.ABSOLUTE}
 					nav_alignment={Header.NavAlignment.RIGHT}
 					logo='static/rgm-logo.png'
@@ -103,7 +102,7 @@ class Projects extends Component {
 					title='Projects'
 					text_position={HeroBanner.TextPosition.BOTTOM}
 					text_gradient={HeroBanner.TextGradient.BOTTOM}
-					background_image='https://biggica-sites.s3.amazonaws.com/rgm/homepage-portfolio/building-03.png'
+					background_image='https://biggica-sites.s3.amazonaws.com/rgm/homepage-portfolio/building-02.png'
 					background_position={HeroBanner.BackgroundPosition.TOP}
 					background_gradient={HeroBanner.BackgroundGradient.NONE}
 					hero_height={HeroBanner.HeroHeight.FULL}	
